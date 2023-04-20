4673e94200a665e563c07fac274608b24215f573
Nicholas McKee <nicholas.mckee@mail.utoronto.ca>
2023-04-20 10:29:51 -0400

    update to enviroment

--- src/get_comp_psych.py
+++ src/get_comp_psych.py
@@ -1,14 +1,33 @@
 import requests
+import competitor
 
-
-def get_wcif(competition: str, event: str):
+def get_wcif(competition: str, event: str, event_type: str):
     url = "https://www.worldcubeassociation.org/api/v0/competitions/"\
           + competition + "/wcif/public"
     response = requests.get(url)
-    for person in response.json()["persons"]:
-        print(person["name"], person["registration"])
+    competitors_in_event = []
+    for person in response.json()['persons']:
+        if person['registration'] is not None \
+                and event in person['registration']['eventIds']:
+            rank = get_pb_rank(person, event, event_type)
+            if rank is not None:
+                comp = competitor.Competitor(person['name'],
+                                             person['wcaId'],
+                                             rank)
+                competitors_in_event.append(comp)
+    return competitors_in_event
+
+
+def get_pb_rank(person, event, event_type):
+    for result in person['personalBests']:
+        if result['eventId'] == event\
+                and result['type'] == event_type:
+            return result['worldRanking']
 
 
 if __name__ == "__main__":
-    get_wcif("PleaseBeSolvedPickering2023", "333bf")
+    comps = get_wcif("PleaseBeSolvedPickering2023", "333bf", "average")
+    comps.sort(key=lambda x: x.rank)
+    for comp in comps:
+        print(comp)
 
---
+++ src/main.py
@@ -0,0 +1,5 @@
+def calculate_odds(comp: str, event: str, event_type: str, num_consider=16):
+
+
+if __name__ == "__main__":
+    calculate_odds()
